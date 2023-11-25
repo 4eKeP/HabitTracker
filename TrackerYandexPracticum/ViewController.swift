2bedfb8e6712bb2bcf262fb91516f31e96a26f24
Egor <egor_chek@mail.ru>
2023-11-25 16:34:30 +0400

    сделан экран запуска и navigation bar с кнопкой добавить

--- TrackerYandexPracticum/ViewController.swift
+++ TrackerYandexPracticum/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            self.view.backgroundColor = UIColor.white
+            makeNavBar()
+        }
+
+    func makeNavBar() {
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: nil, action: nil)
     }
 
 
