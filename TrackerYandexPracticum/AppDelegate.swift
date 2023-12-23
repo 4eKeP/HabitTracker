fd2fda01549805fd026f5d6ff602e0ab1560d720
Egor <egor_chek@mail.ru>
2023-12-23 11:42:38 +0400

    методы CoreData добавленны в AppDelegate

--- TrackerYandexPracticum/AppDelegate.swift
+++ TrackerYandexPracticum/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -35,6 +36,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+//MARK: - CoreData
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Tracker")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                assertionFailure("не удалось загрузить PersistentStores с ошибкой \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                assertionFailure("не удалось загрузить PersistentStores с ошибкой \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
 }
 
