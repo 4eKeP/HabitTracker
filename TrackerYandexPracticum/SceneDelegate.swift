588b1a24e22268030ea101adbe3d61316a4587d3
Egor <egor_chek@mail.ru>
2024-01-13 21:23:52 +0400

    онбордин голностью возвращен из потеряной ветки

--- TrackerYandexPracticum/SceneDelegate.swift
+++ TrackerYandexPracticum/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowsScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowsScene)
-        window.rootViewController = TabBarController()
+        window.rootViewController = UserDefaults.standard.isOnBoarded ? TabBarController() : OnBoardingController()
         self.window = window
         window.makeKeyAndVisible()
     }
